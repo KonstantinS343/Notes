c803b3315aa937f9b2d4d6c413866bedd5dacd27
KonstantinS343 <mihailrudkovic@gmail.com>
2023-07-26 20:53:00 +0300

    Added CORS settings

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from redis import asyncio as aioredis
 from fastapi_cache import FastAPICache
@@ -40,6 +41,16 @@ app.include_router(
 
 app.include_router(verify_router)
 app.include_router(reset_password_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 
 @app.on_event('startup')
