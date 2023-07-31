571a628376ca5406dab58765c8c4f5da277d61b4
KonstantinS343 <mihailrudkovic@gmail.com>
2023-07-31 18:54:57 +0300

    Fixed test fail

--- tests/task/api/test_check_empty_db.py
+++ tests/task/api/test_check_empty_db.py
@@ -3,8 +3,8 @@ from httpx import AsyncClient
 
 async def test_get_all_tasks_empty(async_client: AsyncClient, create_user):
     response = await async_client.post('/auth/jwt/login', data={
-        'username': 'admin@gmail.com',
-        'password': 'admin'
+        "username": "admin@gmail.com",
+        "password": "admin"
     })
 
     cookies = response.cookies.get('note')
